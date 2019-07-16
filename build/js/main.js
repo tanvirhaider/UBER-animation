836bb0ef9713d731717a5dafa3b50a0055a6b2a2
tanvir haider <tanvir@willow.studio>
2019-07-15 23:34:05 -0400

    new

---
+++ build/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+console.log("this is the main function");
+
+function animateBox1() {
+  TweenLite.set("#box-1-minutehand", {
+    transformOrigin: "left 50%",
+    rotation: -90
+  });
+  TweenLite.set("#box-1-hourhand", {
+    transformOrigin: "left 50%",
+    rotation: -90
+  });
+  TweenLite.to("#box-1-minutehand", 15, {
+    ease: Power0.easeNone,
+    rotation: 760
+  });
+  TweenLite.to("#box-1-hourhand", 15, {
+    ease: Power0.easeNone,
+    rotation: 300
+  });
+}
+
+function animateBox3() {
+  //  clip: rect(80px, 250px, 250px, 130px);
+  TweenLite.set("#bag-fill", {
+    clip: "rect(250px, 250px, 250px, 130px"
+  });
+  TweenLite.to("#bag-fill", 10, {
+    ease: Power0.easeNone,
+    clip: "rect(80px, 250px, 250px, 130px"
+  });
+}
+
+animateBox1();
+animateBox3();
+
+//# sourceMappingURL=main.js.map
